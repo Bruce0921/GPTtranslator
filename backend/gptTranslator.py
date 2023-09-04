d6a922923b2cd303215f83eff9d7a876893cb11a
Chi Cheng <114181725+Bruce0921@users.noreply.github.com>
2023-09-04 13:40:21 -0700

    Update gptTranslator.py

--- backend/gptTranslator.py
+++ backend/gptTranslator.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 
 import os
-if not os.environ.get("OPENAI_API_KEY"):
-    raise EnvironmentError("OPENAI_API_KEY not set")
-    
 import openai
 import logging
 logging.basicConfig(filename='error.log', level=logging.DEBUG)
@@ -11,7 +8,10 @@ logging.basicConfig(filename='error.log', level=logging.DEBUG)
 app = Flask(__name__, static_folder='../frontend')
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
-
+@app.route('/test', methods=['GET'])
+def test():
+    return "Test endpoint"
+    
 @app.route('/')
 def home():
     return send_from_directory(app.static_folder, 'index.html')
